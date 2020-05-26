d4649f81f0621d4d9d3ca9a68e4fb5eb8baaff87
hugo-espinelli <hugoespinelli@gmail.com>
2020-05-26 02:23:20 -0300

    fix setup.py increment version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pythocrypt',
-      version='0.3',
+      version='0.4',
       description='Several ciphers for python',
       long_description=long_description,
       long_description_content_type="text/markdown",
